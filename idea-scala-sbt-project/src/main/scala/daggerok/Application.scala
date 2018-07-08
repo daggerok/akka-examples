b4476937d31e2dd0dd0d51a0b40e3d5aa7fb5787
Maksim Kostromin <daggerok@gmail.com>
2018-07-09 00:50:45 +0300

    I transformed a bug into a feature. Once you learn how, you'll never forget it (C) whatthecommit.com

--- idea-scala-sbt-project/src/main/scala/daggerok/Application.scala
+++ idea-scala-sbt-project/src/main/scala/daggerok/Application.scala
@@ -6,17 +6,16 @@ case class MyMessage(name: String)
 
 class MyActor extends Actor {
   override def receive: Receive = {
-    case MyMessage(name) => println(s"Hello, $name!")
+    case HelloMessage(name) => println(s"Hello, $name!")
   }
 }
 
 object Application {
   def main(args: Array[String]): Unit = {
-
     val mySystem = ActorSystem("my-actor-system")
-    val myActor = mySystem.actorOf(Props[MyActor], "my-actor")
+    val myActor = mySystem.actorOf(Props[HelloActor], "my-actor")
 
-    myActor ! MyMessage("Максимко")
+    myActor ! HelloMessage("Максимко")
     Thread.sleep(1000)
     mySystem.terminate()
   }
