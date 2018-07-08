463442b2019cc360288f9b1abfc81456b87ab1cb
Maksim Kostromin <daggerok@gmail.com>
2018-07-09 00:21:26 +0300

    update idea project | 8==========D (C) whatthecommit.com

--- idea-scala-sbt-project/src/main/scala/daggerok/Application.scala
+++ idea-scala-sbt-project/src/main/scala/daggerok/Application.scala
@@ -1,7 +1,23 @@
 package daggerok
 
+import akka.actor.{Actor, ActorSystem, Props}
+
+case class MyMessage(body: String)
+
+class MyActor extends Actor {
+  override def receive: Receive = {
+    case MyMessage(body) => println(s"Hello, $body!")
+  }
+}
+
 object Application {
   def main(args: Array[String]): Unit = {
-    println("hola!")
+
+    val mySystem = ActorSystem("my-actor-system")
+    val myActor = mySystem.actorOf(Props[MyActor], "my-actor")
+
+    myActor ! MyMessage("Akka")
+    Thread.sleep(1000)
+    mySystem.terminate()
   }
 }
