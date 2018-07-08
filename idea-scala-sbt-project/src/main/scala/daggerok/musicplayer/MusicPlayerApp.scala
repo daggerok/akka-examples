e7a9eb01e4f427880f6988b604acd6713c270fab
Maksim Kostromin <daggerok@gmail.com>
2018-07-09 01:18:33 +0300

    pam anderson is going to love me. (C) whatthecommit.com

---
+++ idea-scala-sbt-project/src/main/scala/daggerok/musicplayer/MusicPlayerApp.scala
@@ -0,0 +1,48 @@
+package daggerok.musicplayer
+
+import akka.actor.{Actor, ActorSystem, Props}
+import daggerok.MusicController.{PlayMsg, StopMsg}
+import daggerok.MusicPlayer.{StartMusicMsg, StopMusicMsg}
+
+object MusicController {
+  sealed trait MusicControllerMessage
+  case object PlayMsg extends MusicControllerMessage
+  case object StopMsg extends MusicControllerMessage
+
+  def props = Props[MusicControllerActor]
+}
+
+class MusicControllerActor extends Actor {
+  override def receive: Receive = {
+    case PlayMsg => println("playing music...")
+    case StopMsg => println("music is stopped")
+  }
+}
+
+object MusicPlayer {
+  sealed trait MusicPlayerMessage
+  case object StopMusicMsg extends MusicPlayerMessage
+  case object StartMusicMsg extends MusicPlayerMessage
+}
+
+class MusicPlayerActor extends Actor {
+  override def receive: Receive = {
+    case StopMusicMsg => println("I don't wanna stop!")
+    case StartMusicMsg =>
+      val ctrlActor = context.actorOf(MusicController.props, "ctrl-actor")
+      ctrlActor ! PlayMsg
+    case _ => println("received unknown message.")
+  }
+}
+
+object MusicPlayerApp {
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("music-system")
+    val actor = system.actorOf(Props[MusicPlayerActor], "mp-actor")
+
+    actor ! StartMusicMsg
+    actor ! StopMusicMsg
+    Thread.sleep(1000)
+    system.terminate()
+  }
+}
