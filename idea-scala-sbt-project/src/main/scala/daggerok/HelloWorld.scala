18f1abe843187146370a0f8a5c663315704029f8
Maksim Kostromin <daggerok@gmail.com>
2018-07-09 00:50:49 +0300

    oops (C) whatthecommit.com

---
+++ idea-scala-sbt-project/src/main/scala/daggerok/HelloWorld.scala
@@ -0,0 +1,22 @@
+package daggerok
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+case class HelloMessage(name: String)
+
+class HelloActor extends Actor {
+  override def receive: Receive = {
+    case HelloMessage(name) => println(s"Hello, $name!")
+  }
+}
+
+object HelloWorldApp {
+  def main(args: Array[String]): Unit = {
+    val helloSystem = ActorSystem("hello-actor-system")
+    val helloActor = helloSystem.actorOf(Props[HelloActor], "hello-actor")
+
+    helloActor ! HelloMessage("Максимко")
+    Thread.sleep(1000)
+    helloSystem.terminate()
+  }
+}
