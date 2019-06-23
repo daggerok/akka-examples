ddabfa22ed3294e24275a91cf80053ca8ec1117b
Maksim Kostromin <daggerok@gmail.com>
2019-06-23 21:13:32 +0300

    Uopdate logger example.

--- a-logger-example/src/main/scala/com/github/daggerok/akka/Main.scala
+++ a-logger-example/src/main/scala/com/github/daggerok/akka/Main.scala
@@ -1,9 +1,42 @@
 package com.github.daggerok.akka
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.event.Logging
+
+class ActorSystemLogger extends Actor {
+  private val log = context.system.log
+  override def receive: Receive = {
+    case message => log.info("{} {} {}",
+      self.path, getClass.getSimpleName, message)
+  }
+}
+
+class ActorWithExplicitLogger extends Actor {
+  private val log = Logging(context.system, this)
+  override def receive: Receive = {
+    case message => log.info("{} {} {}",
+      self.path, getClass.getSimpleName, message)
+  }
+}
+
+class ActorWithLoggingTrait extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case message => log.info("{} {} {}",
+      self.path, getClass.getSimpleName, message)
+  }
+}
 
 object Main extends App {
-  val system = ActorSystem("simple")
-  system.log.info(system.name)
+  private val system = ActorSystem("simple")
+
+  private val actorSystemLogger: ActorRef = system.actorOf(Props[ActorSystemLogger])
+  actorSystemLogger ! System.nanoTime()
+
+  private val actorWithExplicitLogger: ActorRef = system.actorOf(Props[ActorWithExplicitLogger])
+  actorWithExplicitLogger ! System.nanoTime()
+
+  private val actorWithLoggingTrait: ActorRef = system.actorOf(Props[ActorWithLoggingTrait])
+  actorWithLoggingTrait ! System.nanoTime()
+
   system.terminate()
 }
