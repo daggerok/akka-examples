5107b768e9a5118ec59db233de0e5a1429f5eef4
Maksim Kostromin <daggerok@gmail.com>
2019-06-23 14:24:52 +0300

    Add a-child-actor-example.

---
+++ a-child-actor-example/README.md
@@ -0,0 +1,22 @@
+# A Simple Logger Example
+A simple akka getting started example quickly created in idea IDE (sbt wrapper, scala 2.13, akka 2.6.x + testkit, scalatest, scalactic)
+
+_test_ and _run_ app:
+
+```bash
+./sctw test
+./sctw clean run
+```
+
+_build_ fat jar:
+
+```bash
+./sbtw clean assembly
+# ...
+java -jar ./target/scala-*/*-assembly-*.jar
+```
+
+resources:
+
+* [sbt-assembly plugin](https://github.com/sbt/sbt-assembly)
+* [Creating Scala Fat Jars on SBT with sbt-assembly Plugin](http://queirozf.com/entries/creating-scala-fat-jars-for-spark-on-sbt-with-sbt-assembly-plugin)
---
+++ a-child-actor-example/src/main/scala/com/github/daggerok/akka/Main.scala
@@ -0,0 +1,43 @@
+package com.github.daggerok.akka
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+class Child extends Actor {
+  override def receive: Receive = {
+    case message => context.system.log.info("{} got message: {}", self.path, message)
+  }
+}
+
+object Patent {
+  case class CreateChildActor(name: String)
+  case class TellToChild(message: String)
+}
+
+class Patent extends Actor {
+  import Patent._
+
+  override def receive: Receive = withoutChild
+
+  def withoutChild: Receive = {
+    case CreateChildActor(name) =>
+      val childRef = context.actorOf(Props[Child], name)
+      context become withChild(childRef)
+  }
+
+  def withChild(childRef: ActorRef): Receive = {
+    case TellToChild(message) => childRef forward message
+    case message => context.system.log.warning("received unknown message: {}", message)
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("parent-child-system")
+  val parent: ActorRef = system.actorOf(Props[Patent], "parent")
+
+  import Patent._
+  parent ! CreateChildActor("child")
+  parent ! "Hola!"
+  parent ! TellToChild("Hello!")
+
+  system.terminate()
+}
