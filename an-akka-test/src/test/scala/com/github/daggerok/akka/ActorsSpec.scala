b317737da1b69c617bf4efd8d4692e5da771dc05
Maksim Kostromin <daggerok@gmail.com>
2019-06-24 03:47:36 +0300

    Improve Probe tests.

--- an-akka-test/src/test/scala/com/github/daggerok/akka/ActorsSpec.scala
+++ an-akka-test/src/test/scala/com/github/daggerok/akka/ActorsSpec.scala
@@ -101,18 +101,43 @@ class ActorsSpec extends TestKit(ActorSystem("system-under-test"))
   }
 
   "A TestProbe" should {
-    val probe = TestProbe("FirstBlood")
-
     "send message to default testActor" in {
+      val probe = TestProbe("FirstBlood")
       val message = "Hello, testActor!"
       probe.send(testActor, message)
       expectMsg(message)
     }
 
     "receive a message from default testActor" in {
+      val probe = TestProbe("FirstBlood")
       val message = "Hello, probe.ref!"
       probe.ref ! message
       probe.expectMsg(message)
+    }
+
+    "implement probe.receiveWhile partial function and reply back to testActor" in {
+      val probe = TestProbe("FirstBlood")
+      probe.ref ! "Hola!"
+      val strings = probe.receiveWhile() {
+        case string: String => testActor ! s"$string $string".trim
+      }
+      probe.reply(strings)
+      expectMsgAnyOf("Hola! Hola!")
+    }
+
+    "direct send / replay between probe actors using probe receiveWhile partial function implementation" in {
+      val first = TestProbe("1st")
+      val second = TestProbe("2nd")
+
+      //(second.ref ! "Hola!")(first.ref) // same as:
+      first.send(second.ref, "Hola!")
+
+      import concurrent.duration._
+      second.receiveWhile(max = 1.seconds, messages = 1) {
+        case string: String => first.ref ! s"$string $string".trim
+      }
+
+      first.expectMsg("Hola! Hola!")
     }
   }
 }
